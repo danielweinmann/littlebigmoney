f7fd599281764ab401e44ceaa2b5febcc7a46a3c
Daniel Weinmann <danielweinmann@gmail.com>
2013-08-13 22:27:15 -0300

    Debugging blog still

--- lib/blog.rb
+++ lib/blog.rb
@@ -3,6 +3,7 @@ class Blog
     def fetch_last_posts
       # Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
         # begin
+          raise "Hey! = #{::Configuration[:blog_url]}/feeds/posts/default?alt=rss"
           feed = Feedzirra::Feed.fetch_and_parse("#{::Configuration[:blog_url]}/feeds/posts/default?alt=rss")
           feed.entries
         # rescue
