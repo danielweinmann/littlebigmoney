dfc675ac4c1fa53b30fc57b03aab27f70500eee9
Daniel Weinmann <danielweinmann@gmail.com>
2013-08-17 01:59:06 -0300

    Fixes timezone problems in Project scopes

--- app/models/project.rb
+++ app/models/project.rb
@@ -55,13 +55,13 @@ class Project < ActiveRecord::Base
 
   scope :near_of, ->(address_state) { joins(:user).where("lower(users.address_state) = lower(?)", address_state) }
   scope :visible, where("projects.state NOT IN ('draft', 'rejected', 'deleted')")
-  scope :financial, where("((projects.expires_at) > (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) - '15 days'::interval) AND (state in ('online', 'successful', 'waiting_funds'))")
+  scope :financial, where("((projects.expires_at AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) > (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) - '15 days'::interval) AND (state in ('online', 'successful', 'waiting_funds'))")
   scope :recommended, where(recommended: true)
-  scope :expired, where("(projects.expires_at) < (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota'))")
-  scope :not_expired, where("(projects.expires_at) >= (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota'))")
-  scope :expiring, not_expired.where("(projects.expires_at) <= ((current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) + interval '2 weeks')")
-  scope :not_expiring, not_expired.where("NOT ((projects.expires_at) <= ((current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) + interval '2 weeks'))")
-  scope :recent, where("(current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) - projects.online_date <= '5 days'::interval")
+  scope :expired, where("(projects.expires_at AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) < (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota'))")
+  scope :not_expired, where("(projects.expires_at AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) >= (current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota'))")
+  scope :expiring, not_expired.where("(projects.expires_at AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) <= ((current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) + interval '2 weeks')")
+  scope :not_expiring, not_expired.where("NOT ((projects.expires_at AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) <= ((current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) + interval '2 weeks'))")
+  scope :recent, where("(current_timestamp AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) - (projects.online_date AT TIME ZONE coalesce((SELECT value FROM configurations WHERE name = 'timezone'), 'America/Bogota')) <= '5 days'::interval")
   scope :successful, where(state: 'successful')
   scope :online, where(state: 'online')
   scope :recommended_for_home, ->{
