9a707208e1c995df897b8019dcfc3ea91e9c2753
Daniel Weinmann <danielweinmann@gmail.com>
2013-09-08 20:28:59 -0300

    Changing the minimum back to USD 5

--- spec/models/backer_spec.rb
+++ spec/models/backer_spec.rb
@@ -22,7 +22,7 @@ describe Backer do
     it{ should validate_presence_of(:project) }
     it{ should validate_presence_of(:user) }
     it{ should validate_presence_of(:value) }
-    it{ should_not allow_value(9.99).for(:value) }
+    it{ should_not allow_value(4.99).for(:value) }
     it{ should allow_value(10).for(:value) }
     it{ should allow_value(20).for(:value) }
   end
