ac1222eb6e3b2d6a3e1021b1648943574f46e046
Daniel Weinmann <danielweinmann@gmail.com>
2013-12-05 11:25:53 -0200

    Making project feature spec wait more before checking for searched projects

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -48,7 +48,7 @@ describe "Projects" do
       FactoryGirl.create(:project, name: 'Foo', state: 'online', online_days: 30, recommended: true)
       FactoryGirl.create(:project, name: 'Lorem', state: 'online', online_days: 30, recommended: false)
       visit explore_path(anchor: :search) + '/Lorem'
-      sleep 4
+      sleep 10
     end
     it "should show searched projects" do
       searched = all(".collection_list .project")
