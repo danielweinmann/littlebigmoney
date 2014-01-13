94e4be23d40ce8a92c03edadd8e8d0d08115636a
Daniel Weinmann <danielweinmann@gmail.com>
2014-01-13 18:48:02 -0300

    Making Configuration look for currency conversion on SuperFinanciera

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,5 +1,36 @@
 class Configuration < ActiveRecord::Base
   validates_presence_of :name
+  
+  def self.update_configurations!
+    self.update_paypal_conversion!
+  end
+  
+  def self.update_paypal_conversion!
+    page = Nokogiri::HTML(HTTParty.get("http://www.superfinanciera.gov.co/Cifras/informacion/diarios/tcrm/tcrm.htm").body)
+    
+    mean = 0
+    volatility = 0
+
+    page.search("table").search("tr").each do |row|
+      variable = row.search("td")[0]
+      variable = variable.text.strip if variable
+      value = row.search("td")[2]
+      value = value.text.strip if value
+      if variable == 'Promedio Ponderado de Venta'
+        mean = value.gsub(',', '').to_f
+      elsif variable == 'Volatilidad de la TCRM (%)'
+        volatility = value.gsub('%', '').to_f / 100
+      end
+    end
+    
+    if mean != 0 && volatility != 0
+      conversion = ((mean - mean * volatility) * 100).round.to_f / 100
+      Configuration[:paypal_currency] = "USD"
+      Configuration[:paypal_conversion] = conversion
+    end
+    
+  end
+  
   class << self
     # This method returns the values of the config simulating a Hash, like:
     #   Configuration[:foo]
