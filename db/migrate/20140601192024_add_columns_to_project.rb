2c92319fe0f046e8f189bae46cbee6d17143ba09
Daniel Weinmann <danielweinmann@gmail.com>
2014-06-05 16:56:24 -0300

    Fixing migration for Travis

--- db/migrate/20140601192024_add_columns_to_project.rb
+++ db/migrate/20140601192024_add_columns_to_project.rb
@@ -3,7 +3,7 @@ class AddColumnsToProject < ActiveRecord::Migration
     add_column :projects, :history, :text
     add_column :projects, :cause, :text
     add_column :projects, :description, :text
-    change_column :projects, :impact,  :text
+    add_column :projects, :impact,  :text
     add_column :projects, :budget, :text
     add_column :projects, :implementation, :text
   end
