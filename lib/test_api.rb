45cfdde41d277cbd7e8b1c0fa3c3ceb6d11c1969
Daniel Weinmann <danielweinmann@gmail.com>
2014-06-06 16:49:56 -0300

    Updating test_api

--- lib/test_api.rb
+++ lib/test_api.rb
@@ -4,20 +4,22 @@ require 'open-uri'
 
 api_key = ARGV[0]
 
-response = HTTParty.post "http://localhost:3000/es/users/authenticate_user.json", body: URI::encode("user[email]=testuser@gmail.com&user[name]=Test User Name"), headers: { "Authorization" => "Token token=\"#{api_key}\"" }
+host = "littlebigmoney.org"
+email = "testuser@gmail.com"
+name = "Test User Name"
+project = "52"
+value = "25000"
+reward = "132"
+anonymous = "1"
+
+response = HTTParty.post "http://#{host}/es/users/authenticate_user.json", body: URI::encode("user[email]=#{email}&user[name]=#{name}"), headers: { "Authorization" => "Token token=\"#{api_key}\"" }
 
 if response.code == 200
-
   access_token = JSON.parse(response.body)["access_token"]
   puts access_token
-
-  response = HTTParty.post "http://localhost:3000/es/projects/52/backers", body: URI::encode("backer[value]=25000&backer[reward_id]=132&backer[anonymous]=1"), headers: { "Authorization" => "Token token=\"#{access_token}\"" }
-
+  response = HTTParty.post "http://#{host}/es/projects/#{project}/backers", body: URI::encode("backer[value]=#{value}&backer[reward_id]=#{reward}&backer[anonymous]=#{anonymous}"), headers: { "Authorization" => "Token token=\"#{access_token}\"" }
   puts response.body.match(/<title>(.+)<\/title>/)[1]
-
 else
-
   puts "Error!"
   puts response
-
 end
